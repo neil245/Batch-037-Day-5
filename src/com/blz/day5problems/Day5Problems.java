017061825c92feb9db0011bcfefb4a63e69a1a2c
neil245 <indranilghoshal5@gmail.com>
2021-08-01 01:55:30 +0530

    Powe of 2

--- src/com/blz/day5problems/Day5Problems.java
+++ src/com/blz/day5problems/Day5Problems.java
@@ -1,35 +1,23 @@
 package com.blz.day5problems;
-
-import java.util.Random;
 import java.util.Scanner;
-
 public class Day5Problems {
-	public static void main(String[] args) {
-		long year;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Enter a year : ");
-		year = sc.nextInt();
-		if(year%4==0){
-			if(year%100 ==0) {
-				if(year%400 == 0) {
-					System.out.println("It is a leap year");
-				}
-				else {
-					System.out.println("It is not a leap year");
-				}
-				}
-			else {
-				System.out.println("It is not a leap year");
-			}
-		}
-			else {
-				System.out.println("It is not a leap year");
-						
-			}
-			
-							
+	 public static void main(String[] args) {
 
-		}
+	 Scanner sc = new Scanner(System.in);
+	        System.out.println("Enter a number");
+	        int n = sc.nextInt();
+		 
+	        int i = 0;               
+	        int powerOfTwo = 1;       
+
+	        
+	        while (i <= n) {
+	            System.out.println(i + " " + powerOfTwo);   
+	            powerOfTwo = 2 * powerOfTwo;                
+	            i = i + 1;
+	        }
+
+	    }
 	}
 
 
