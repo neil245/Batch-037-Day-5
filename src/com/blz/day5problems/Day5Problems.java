03c57a94051e8deb20edc3d52c62a570c805f010
neil245 <indranilghoshal5@gmail.com>
2021-08-01 01:32:35 +0530

    Leap year program added

--- src/com/blz/day5problems/Day5Problems.java
+++ src/com/blz/day5problems/Day5Problems.java
@@ -1,16 +1,35 @@
 package com.blz.day5problems;
+
 import java.util.Random;
+import java.util.Scanner;
+
 public class Day5Problems {
 	public static void main(String[] args) {
-		//Coin-Simulation
-		int coin = (int)Math.floor(Math.random()*2);
-		System.out.println(coin);
-		if(coin == 1) {
-			System.out.println("Heads");
+		long year;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a year : ");
+		year = sc.nextInt();
+		if(year%4==0){
+			if(year%100 ==0) {
+				if(year%400 == 0) {
+					System.out.println("It is a leap year");
+				}
+				else {
+					System.out.println("It is not a leap year");
+				}
+				}
+			else {
+				System.out.println("It is not a leap year");
+			}
 		}
-		else {
-			System.out.println("Tails");
+			else {
+				System.out.println("It is not a leap year");
+						
+			}
+			
+							
+
 		}
 	}
 
-}
+
