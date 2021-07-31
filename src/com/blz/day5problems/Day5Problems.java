ccf17c5a72655563b68ede59b53546d473888ce7
neil245 <indranilghoshal5@gmail.com>
2021-08-01 03:25:46 +0530

    Odd-Even program

--- src/com/blz/day5problems/Day5Problems.java
+++ src/com/blz/day5problems/Day5Problems.java
@@ -4,19 +4,15 @@ public class Day5Problems {
 	 public static void main(String[] args) {
 
 	 Scanner sc = new Scanner(System.in);
-	        System.out.println("Enter the value of a : ");
-	        int a  = sc.nextInt();
-	        System.out.println("Enter the value of b : ");
-	        int b = sc.nextInt();
-	        System.out.println("a : "+a);
-	        System.out.println("b : "+b);
-	        int temp = a;
-	        a = b;
-	        b = temp;
-	        System.out.println("The values of a and b after swapping is...");
-	        System.out.println("a : "+a);
-	        System.out.println("b : "+b);
-	       
+	        System.out.println("Enter a number : ");
+	        int n = sc.nextInt();
+	        System.out.println("The number is : "+n);
+	        if(n%2 ==0) {
+	        	System.out.println("The number is even");
+	        }
+	        else {
+	        	System.out.println("The number is odd");
+	        }
 	       
 	        }
 	       
