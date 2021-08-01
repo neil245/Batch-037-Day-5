a7d7486939915e7e707328a83866d79638f45330
neil245 <indranilghoshal5@gmail.com>
2021-08-01 10:52:12 +0530

    Largest numbers logic implemented

--- src/com/blz/day5problems/Day5Problems.java
+++ src/com/blz/day5problems/Day5Problems.java
@@ -4,18 +4,28 @@ public class Day5Problems {
 	 public static void main(String[] args) {
 
 	 Scanner sc = new Scanner(System.in);
-	        char ch ;
-	        System.out.println("Enter an alphabet");
-	        ch = sc.next().charAt(0);
-	        System.out.println(ch);
-	        if(ch=='a' || ch == 'e' ||ch =='i'|| ch=='o' || ch =='u' || ch=='A' || ch=='E'  || ch=='I' || ch=='O' || ch =='U') {
-	        	System.out.println("The entered character is a vowel");
-	        }
-	        else {
-	        	System.out.println("The entered character is consonant");
-	        }
-	       
-	        }
+	 System.out.println("Enter the first number");
+	 int a = sc.nextInt();
+	 System.out.println("Enter the first number");
+	 int b= sc.nextInt();
+	 System.out.println("Enter the first number");
+	 int c= sc.nextInt();
+	 System.out.println("The first number is : "+a);
+	 System.out.println("The second is : "+b);
+	 System.out.println("The third is : "+c);
+	 if(a>b && a>c) {
+		 System.out.println(a+" is the largest");}
+		 else if(b>a && b>c) {
+			 System.out.println(b+" is the largest");
+		 }
+		 else {
+			 System.out.println(c+" is the largest");
+		 }
+		 		
+	 
+	 
+	 
+	          }
 	       
 	    }
 	
