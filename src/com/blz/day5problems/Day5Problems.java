9c9f5d7c7721143523949428937b5b7ffa3f3dd0
neil245 <indranilghoshal5@gmail.com>
2021-08-01 11:26:45 +0530

    2D array with print writer

--- src/com/blz/day5problems/Day5Problems.java
+++ src/com/blz/day5problems/Day5Problems.java
@@ -1,33 +1,42 @@
 package com.blz.day5problems;
 import java.util.Scanner;
+import java.io.*;
 public class Day5Problems {
 	 public static void main(String[] args) {
-
+		int[][] arr = new int[5][5];
 	 Scanner sc = new Scanner(System.in);
-	 System.out.println("Enter the first number");
-	 int a = sc.nextInt();
-	 System.out.println("Enter the first number");
-	 int b= sc.nextInt();
-	 System.out.println("Enter the first number");
-	 int c= sc.nextInt();
-	 System.out.println("The first number is : "+a);
-	 System.out.println("The second is : "+b);
-	 System.out.println("The third is : "+c);
-	 if(a>b && a>c) {
-		 System.out.println(a+" is the largest");}
-		 else if(b>a && b>c) {
-			 System.out.println(b+" is the largest");
+	 System.out.println("Enter the number of rows");
+	 int row = sc.nextInt();
+	 System.out.println("Enter the number of columns");
+	 int col = sc.nextInt();
+	 for(int i=0;i<row;i++) {
+		 for(int j=0;j<col;j++) {
+			 System.out.println("Enter the "+i+j+" element");
+			arr[i][j]  = sc.nextInt();
 		 }
-		 else {
-			 System.out.println(c+" is the largest");
+	 }
+//	 for(int i=0;i<row;i++) {
+//		 for(int j=0;j<col;j++) {
+//			 System.out.print(arr[i][j]+" ");
+//		 }
+//		 System.out.println();
+//	 
+//	          }
+	 PrintWriter pw = new PrintWriter(System.out);
+//	 pw.print(arr);
+//	 pw.flush();
+	 for(int i=0;i<row;i++) {
+		 for(int j=0;j<col;j++) {
+	
+			 pw.print(arr);
 		 }
-		 		
-	 
-	 
+	System.out.println();
+		
 	 
 	          }
 	       
 	    }
+}
 	
 
 
